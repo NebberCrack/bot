e88d5367f7715305b2d4509a6590d55924e142e5
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-06-30 20:58:42 -0500

    Add files via upload

--- plugins/menu-ok.js
+++ plugins/menu-ok.js
@@ -2,10 +2,8 @@ let handler = async (m, { text }) => {
 let user = global.db.data.users[m.sender]
 m.reply(`HOLA ${conn.getName(m.sender)} ESTE ES EL MENU\n
 ╭━━〔 𝗠𝗘𝗡𝗨 𝗗𝗘𝗟 𝗕𝗢𝗧 〕
-
-╭━〔 𝙅𝙐𝙀𝙂𝙊𝙎 𝘿𝙄𝙉𝘼𝙈𝙄𝘾𝙊𝙎 〕⬣
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃🎡➺ _.mates | matemáticas | math_
+┃🎡➺ _${usedPrefix}mates | matemáticas | math_
 
 ╰━━━━━━━━━━━━━━━━━━━⬣
 
