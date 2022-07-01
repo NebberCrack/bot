54e891985f07f0cf9d271db621f4c63bc911f774
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-06-30 20:45:12 -0500

    Add files via upload

--- plugins/menu-ok.js
+++ plugins/menu-ok.js
@@ -2,11 +2,11 @@ let handler = async (m, { text }) => {
 let user = global.db.data.users[m.sender]
 user.afk = + new Date
 user.afkReason = text
-m.reply(`HOLA ${conn.getName(m.sender)} ESTE ES EL MENU*\n
+m.reply(`HOLA ${conn.getName(m.sender)} ESTE ES EL MENU\n
 MENU DE OPCIONES
 	
 `)}
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
-handler.command = /^menu3$/i
+handler.command = /^menu$/i
 export default handler
