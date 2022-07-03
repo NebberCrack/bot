b08d87a44e79b63e7d59dfd0bfe0ee29de3c6855
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-07-03 12:53:57 -0500

    Add files via upload

--- plugins/menu.js
+++ plugins/menu.js
@@ -433,3 +433,20 @@ let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+function ucapan() {
+  const time = moment.tz('America/Los_Angeles').format('HH')  //America/Los_Angeles  Asia/Jakarta   America/Toronto
+  let res = "👋 *BIENVENIDO(A) | WELCOME* 👋"
+  if (time >= 4) {
+    res = "🌇 *Buenos Días* ⛅"
+  }
+  if (time >= 11) {
+    res = "🏙️ *Buenas Tardes* 🌤️"
+  }
+  if (time >= 15) {
+    res = "🌆 *Buenas tardes* 🌥️"
+  }
+  if (time >= 17) {
+    res = "🌃 *Buenas noches* 💫"
+  }
+  return res
+}
