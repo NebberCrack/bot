555f293cfe83100873d3373a2877198f4a6a090d
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-07-04 23:39:47 -0500

    Add files via upload

--- plugins/grupo-info.js
+++ plugins/grupo-info.js
@@ -5,7 +5,7 @@ const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let text = 
-`╔══════════════\n║*${groupMetadata.subject}*\n╠══════════════
+`╔═══════════════\n║*${groupMetadata.subject}*\n╠═══════════════
 ║✨ 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝘾𝙄𝙊𝙉
 ║${groupMetadata.desc?.toString() || '𝙉𝙊 𝙃𝘼𝙔 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝘾𝙄𝙊𝙉'}
 
