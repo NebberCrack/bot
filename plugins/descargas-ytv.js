c97a9c0e139403679d4c27e7b62625cee4e70fbc
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-07-04 23:07:12 -0500

    Add files via upload

--- plugins/descargas-ytv.js
+++ plugins/descargas-ytv.js
@@ -18,7 +18,7 @@ if (!isY) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
 ╰━━━━━━❰ *𓃠 ${vs}* ❱━━━━━⬣`.trim(), m)
   
 await conn.sendFile(m.chat, link, title + '.mp3', `
-╭━━❰  🎞️ Play - Romina 🥀❱━━⬣
+╭━━❰ 🎞️ Play - Romina 🥀❱━⬣
 ┃ 🔰 𝙏𝙄𝙏𝙐𝙇𝙊
 ┃ ${title}
 ┃ *Disfruta el video*
